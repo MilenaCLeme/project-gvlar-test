c0d343ce26449ba192da86ba067d6fed98d0e32d
MilenaCLeme <milenaleme4@hotmail.com>
2023-01-02 15:56:02 -0300

    feas: start footer

---
+++ front-end/src/components/Footer/Footer.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<footer>
+			<div>
+				<div>
+					<img />
+					<div>
+						<div>
+							<p>R. Cuiabá, 797 - Alto da Mooca, São Paulo - SP</p>
+						</div>
+						<div>
+							<h3>Horário de funcionamento</h3>
+							<p>Horário de Segunda a Sexta</p>
+							<p>09:00hrs as 18:00hrs</p>
+						</div>
+					</div>
+				</div>
+				<div>
+					<h3>Nosso Contato</h3>
+					<div>
+						<p>(11) 94002-0947</p>
+					</div>
+					<div>
+						<p>gvlar@gmail.com</p>
+					</div>
+					<a>Outras formas de contato</a>
+				</div>
+				<div>
+					<h3>Siga-nos</h3>
+					<nav>
+
+					</nav>
+				</div>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
