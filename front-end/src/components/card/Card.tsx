2642b14244d331966e24c6c687a4fae466f4408e
MilenaCLeme <milenaleme4@hotmail.com>
2023-01-03 13:54:17 -0300

    feas: create card model

--- front-end/src/components/card/Card.tsx
+++ front-end/src/components/card/Card.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import style from './Card.module.scss';
 
 function Card() {
 	return (
-		<p>test</p>
+		<div className={style.card}>
+			<div className={style.card__info}>
+				<p>Locação e Venda</p>
+			</div>
+			<img className={style.card__img} src='https://fotos.vivadecora.com.br/decoracao-aquilesnicol-178160-proportional-height_cover_medium.jpg' />
+			<div className={style.card__about}>
+				<h1 className={style.card__about__type}>Casa</h1>
+				<h2 className={style.card__about__address}>Parque na Mooca</h2>
+				<p className={style.card__about__region}>São Paulo - SP</p>
+				<p className={style.card__about__value}>R$1.000.000,00</p>
+			</div>
+		</div>
 	);
 }
 
--- front-end/src/router/index.tsx
+++ front-end/src/router/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+import Card from '../components/card/Card';
 
 function Route() {
 	return (
 		<>
 			<Header />
 			<Footer />
+			<br></br>
+			<Card />
 		</>
 	);
 }
