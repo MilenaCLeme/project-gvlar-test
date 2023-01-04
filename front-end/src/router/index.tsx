d9cad28b52fce93c219a4637fce43dce884e425a
MilenaCLeme <milenaleme4@hotmail.com>
2023-01-04 17:15:59 -0300

    feas: insert home

--- front-end/src/router/index.tsx
+++ front-end/src/router/index.tsx
@@ -3,12 +3,15 @@ import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import ClientPage from '../page/ClientPage/ClientPage';
 import {Routes, Route} from 'react-router-dom';
+import Home from '../page/Home/Home';
 
 function Router() {
 	return (
 		<main className='container'>
 			<Routes>
-				<Route path='/' element={<ClientPage />} />
+				<Route path='/' element={<ClientPage />} >
+					<Route index element={<Home />} />
+				</Route>
 			</Routes>
 		</main>
 	);
