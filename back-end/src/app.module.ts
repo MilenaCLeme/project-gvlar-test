8fda878eee71b4dd90db276918e28d49d91ff4fd
MilenaCLeme <milenaleme4@hotmail.com>
2023-01-20 10:19:50 -0300

    feas: insert back-end start project

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: false,
+      playground: false,
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
