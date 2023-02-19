e72531d3b35cc79e26eae3504fa13d3180125326
MilenaCLeme <milenaleme4@hotmail.com>
2023-02-18 21:27:06 -0300

    feas: new

--- back-end-nestjs/src/app.module.ts
+++ back-end-nestjs/src/app.module.ts
@@ -1,12 +1,11 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { PrismaService } from './prisma/prisma.service';
-import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service.js';
+import { AuthModule } from './auth/auth.module.js';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
-import { AccesTokenGuard } from './auth/guards/acessToken.guard';
+import { AccesTokenGuard } from './auth/guards/acessToken.guard.js';
 import { UserModule } from './user/user.module';
 import { MailModule } from './mail/mail.module';
 import { ImmobileModule } from './immobile/immobile.module';
@@ -16,10 +15,10 @@ import { PhotographModule } from './photograph/photograph.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
+    GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
+      debug: true,
     }),
     AuthModule,
     UserModule,
